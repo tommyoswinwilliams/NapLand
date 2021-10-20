ac312e3774d031a28346deb6645c030cbe5b5002
Anna <aejablonska66@gmail.com>
2021-10-20 12:29:01 +0100

    add authentication

--- app.rb
+++ app.rb
@@ -29,6 +29,24 @@ class NapLand < Sinatra::Base
     redirect '/'
   end
 
+  get '/sessions/new' do
+    erb :"sessions/new"
+  end
+
+  post '/sessions' do
+    if ENV['ENVIRONMENT'] == "test"
+      connection = PG.connect(dbname: 'napland_test')
+    else
+      connection = PG.connect(dbname: 'napland')
+    end
+
+    result = connection.exec_params("SELECT * FROM users WHERE email = $1", [params[:email]])
+    user = User.new(result[0]['id'], result[0]['email'], result[0]['password'])
+
+    session[:user_id] = user.id
+    redirect('/')
+  end
+
   run! if app_file == $0
 
 end
