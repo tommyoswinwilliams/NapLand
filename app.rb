00cb284a55aa3af80cccd966b2b1cb2ec5455c54
twilliams1988 <tom@teow.co.uk>
2021-10-21 11:21:40 +0100

    Add user sign out

--- app.rb
+++ app.rb
@@ -47,6 +47,12 @@ class NapLand < Sinatra::Base
     end
   end
 
+  post '/sessions/destroy' do
+    session.clear
+    flash[:notice] = 'You have signed out.'
+    redirect('/')
+  end
+
   run! if app_file == $0
 
 end
