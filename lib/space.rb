f7ffddad7bb5e3cae937b1d50d71213433b7c1cb
pinar <pnr_ozbakir@msn.com>
2021-10-22 11:48:59 +0100

    availability filter merge ready

--- lib/space.rb
+++ lib/space.rb
@@ -44,4 +44,20 @@ class Space
     result = connection.exec("INSERT INTO spaces(name, description, price, available_from, available_to) VALUES ($1, $2, $3, $4, $5) RETURNING id, name, description, price, available_from, available_to;", [name, description, price, available_from, available_to])
     Space.new(id: result[0]["id"], name: result[0]["name"], description: result[0]["description"], price: result[0]["price"], available_from: result[0]["available_from"], available_to: result[0]["available_to"])
   end
+
+  def self.show_available(date_from:, date_to:)
+    if ENV['ENVIRONMENT'] == "test"
+      connection = PG.connect(dbname: "napland_test")
+    else
+      connection = PG.connect(dbname: "napland")
+    end
+    query = "SELECT * FROM spaces WHERE
+            (available_from BETWEEN CAST('#{date_from}' AS DATE) AND CAST('#{date_to}' AS DATE)) OR 
+            (available_to BETWEEN CAST('#{date_from}' AS DATE) AND CAST('#{date_to}' AS DATE)) OR 
+            (available_from <= CAST('#{date_from}' AS DATE) AND available_to >= CAST('#{date_to}' AS DATE));"
+    result = connection.exec(query)
+    result.map do |item|
+      Space.new(id: item["id"], name: item["name"], description: item["description"], price: item["price"], available_from: item["available_from"], available_to: item["available_to"])
+    end
+  end
 end
---
+++ spec/features/show_available_spec.rb
@@ -0,0 +1,12 @@
+feature "Viewing available listings" do
+  
+  scenario "a user can see available spaces given a date" do
+    visit '/spaces'
+    fill_in :date_from, with: '2021-06-11'
+    fill_in :date_to, with: '2021-06-15'
+    click_button "List Spaces"
+
+    expect(page).not_to have_content '2021-11-11'
+    expect(page).not_to have_content '2021-11-11'
+  end
+end
